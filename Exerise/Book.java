bfe756f36049dbade7575e0dcfaca17fe07e3946
Phumipat <s68122250008@ssru.ac.th>
2026-02-11 16:53:47 +0700

    Create Book.java
    Practice constructor

---
+++ Exerise/Book.java
@@ -0,0 +1,41 @@
+package Exerise;
+
+public class Book {
+    String title;
+    String author;
+    double price;
+
+    // Default Contructor
+    public Book(String title, String author, double price) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    public Book(String title, String author) {
+        this(title, author, 0.0);
+    }
+
+    public Book(String title) {
+        this(title, "unknown", 0.0);
+    }
+
+    public Book() {
+        this("unknown", "unknown", 0.0);
+    }
+
+    // Method Display
+    void showInfo() {
+        System.out.println("Title: " + this.title + " Author: " + this.author + " Price: " + this.price);
+    }
+
+    public static void main(String[] args) {
+        Book b1 = new Book("Java Basics", "John", 250);
+        Book b2 = new Book("OOP", "Anna");
+        Book b3 = new Book();
+
+        b1.showInfo();
+        b2.showInfo();
+        b3.showInfo();
+    }
+}
